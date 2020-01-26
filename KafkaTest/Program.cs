7a4f8c198284d041a9beb1a7f257bbc4cc34c3a3
Tilborg, Stefan van <Stefan.van.Tilborg@ordina.nl>
2020-01-26 18:41:27 +0100

    Add project files.

---
+++ KafkaTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using Confluent.Kafka;
+using System;
+
+namespace KafkaTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092"};
+
+            Action<DeliveryReport<Null, string>> handler = r => Console.WriteLine(!r.Error.IsError ? 
+                $"Delivered message to {r.TopicPartitionOffset}" : $"Delivery error: {r.Error.Reason}");
+
+            using(var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                var stringValue = "";
+                for (int i = 0; i < 100; i++)
+                {
+                    stringValue += "@";
+                    producer.ProduceAsync(topic: "monkeytail-topic", new Message<Null, string> { Value = stringValue });
+                }
+
+                producer.Flush(timeout: TimeSpan.FromSeconds(10));
+            }
+        }
+    }
+}
---
+++ KafkaTestConsumer/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+
+namespace KafkaTestConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //var config = new Dictionary<string, object>
+            //{
+            //    { "group.id", "monkeytail.consumers" },
+            //    { "bootstrap.servers", "localhost:9092" },
+            //    { "enable.auto.commit", "false" }
+            //};
+
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "localhost:9092",
+                GroupId = "monkeytail.consumers",
+                EnableAutoCommit = false
+            };
+
+            using(var consumer = new ConsumerBuilder<Null,string>(config).Build())
+            {
+                consumer.Subscribe("monkeytail-topic");
+
+                CancellationTokenSource cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) => {
+                    e.Cancel = true; // prevent the process from terminating.
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var cr = consumer.Consume(cts.Token);
+                            Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Error occured: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // Ensure the consumer leaves the group cleanly and final offsets are committed.
+                    consumer.Close();
+                }
+            }
+        }
+    }
+}
